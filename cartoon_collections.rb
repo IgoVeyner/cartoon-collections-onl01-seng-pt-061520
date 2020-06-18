fdcafe5716dfbff1a21173d8adf3cee83f604e17
IgoVeyner <igorveyner95@gmail.com>
2020-06-18 09:16:35 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name.each_with_index {|dwarf, index| puts "#{index + 1}.  #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(name)
+  name.map! {|name| name.capitalize}
+  name.map! {|name| name + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(search_this)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  # This was my answer, i should have just used the find include methods
+  # search_this.collect {|array| 
+  #   i = 0
+  #   while i < cheese_types.length
+  #     if cheese_types[i] == array
+  #       return array
+  #     end
+  #     i += 1
+  #   end
+  # }
+  # return nil
+
+  search_this.find do |maybe_cheesey|
+    cheese_types.include?(maybe_cheesey)
+  end
 end
